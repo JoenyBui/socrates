594beebc6e6413937e8fcef8cc4c08ad1b47eb14
buij <buij@dnb.com>
2018-12-03 01:51:41 -0600

    Fix nginx and proxy server.

--- src/plato/app/app/settings.py
+++ src/plato/app/app/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_DIR = os.path.dirname(BASE_DIR)
+ROOT_DIR = os.path.dirname(PROJECT_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -132,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(os.path.join(ROOT_DIR, 'nginx'), 'static')
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_DIR, 'static'),
+    os.path.join(os.path.join(ROOT_DIR, 'nginx'), 'static'),
 ]
